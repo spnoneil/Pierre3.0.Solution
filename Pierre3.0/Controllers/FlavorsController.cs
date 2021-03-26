811ef27a31da66e94b42b5f007f82a47be284d4f
Scott O'Neil <scottieoneil@gmail.com>
2021-03-26 13:06:07 -0700

    update flavor controller

--- Pierre3.0/Controllers/FlavorsController.cs
+++ Pierre3.0/Controllers/FlavorsController.cs
@@ -37,8 +37,35 @@ namespace Bakery.Controllers
       return View();
     }
 
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+      .Include(f => f.JoinEntities)
+      .ThenInclude(join => join.Treat)
+      .FirstOrDefault(f => f.FlavorId == id);
+      return View(thisFlavor);
+    }
 
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(f=> f.FlavorId == id);
+      return View(thisFlavor);
+    }
 
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+ 
   }
 }
